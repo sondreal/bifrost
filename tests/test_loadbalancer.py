094c0fb34674daa35d76a31c0214875b3e12f9f3
sondreal <sondre@villdyr.no>
2022-10-09 10:37:43 +0200

    Implemented tests for Host-based routing

--- tests/test_loadbalancer.py
+++ tests/test_loadbalancer.py
@@ -11,3 +11,17 @@ def client():
 def test_hello(client):
     result = client.get('/')
     assert b'hello' in result.data
+
+
+def test_host_routing_blue(client):
+    result = client.get('(/', headers={'Host': 'www.blue.no'})
+    assert b'This is the blue application server' in result.data
+
+def test_host_routing_green(client):
+    result = client.get('(/', headers={'Host': 'www.green.no'})
+    assert b'This is the green application server' in result.data
+
+def test_host_routing_notfound(client):
+    result = client.get('(/', headers={'Host': 'www.notfound.no'})
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code
