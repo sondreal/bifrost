ff6b670d44d0c603a79d419a5bf096fc1f071005
sondreal <sondre@villdyr.no>
2022-10-09 10:19:57 +0200

    added a test for the loadbalancer before building it TDD

---
+++ tests/test_loadbalancer.py
@@ -0,0 +1,13 @@
+import pytest
+
+from loadbalancer import loadbalancer
+
+@pytest.fixture
+def client():
+    with loadbalancer.test_client() as client:
+        yield client
+
+
+def test_hello(client):
+    result = client.get('/')
+    assert b'hello' in result.data
